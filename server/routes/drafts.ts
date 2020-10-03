f6ce17d15cb6716535e500ea8ed9dbec17b3f65c
itays123 <itays2005@gmail.com>
2020-10-03 14:54:23 +0300

    use API-kit auth middlewares

--- server/routes/drafts.ts
+++ server/routes/drafts.ts
@@ -1,12 +1,25 @@
 import { getDraft, getDraftsList, setDraft } from "../controllers/drafts";
-import { authCheck } from "../middleware/auth-check";
+// I'm using require because those functions have no type decleations
+const { populateUser, verifyUser } = require(
+  "@greenpress/api-kit/user-middlewares",
+);
 
 export default (app) => {
   app
-    .get("/api/drafts", authCheck, getDraftsList)
-    .get("/api/drafts/all/:draftId", authCheck, getDraft)
-    .get("/api/drafts/:contextType/:draftId", authCheck, getDraft)
-    .post("/api/drafts/:contextType", authCheck, setDraft)
-    .put("/api/drafts/:contextType/:draftId", authCheck, setDraft);
+    .get("/api/drafts", populateUser, verifyUser, getDraftsList)
+    .get("/api/drafts/all/:draftId", populateUser, verifyUser, getDraft)
+    .get(
+      "/api/drafts/:contextType/:draftId",
+      populateUser,
+      verifyUser,
+      getDraft,
+    )
+    .post("/api/drafts/:contextType", populateUser, verifyUser, setDraft)
+    .put(
+      "/api/drafts/:contextType/:draftId",
+      populateUser,
+      verifyUser,
+      setDraft,
+    );
   // you can also PUT `/api/drafts/:contextType/new` to add new drafts
 };
